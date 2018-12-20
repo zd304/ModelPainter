beaa46137b861e91eae4e55a669e44664fc9af6f
zd304 <568892758@qq.com>
2018-12-20 17:20:29 +0800

    init

---
+++ Assets/Scripts/PaintBrush.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintBrush : MonoBehaviour
+{
+	public Color mColor = Color.red;
+    public float mRadius = 0.05f;
+	public float mDepth = 0.1f;
+	public float mHandness = 1.0f;
+	public float mNormalScale = 1.0f;
+    public Texture mDecal = null;
+	private float mLastTime;
+
+    public Vector3 Point
+    {
+        set;
+        get;
+    }
+
+    public Vector3 Normal
+    {
+        set;
+        get;
+    }
+
+	void Start()
+    {
+		mLastTime = 0.0f;
+	}
+	
+	void Update()
+    {
+		if (Time.realtimeSinceStartup - mLastTime <= 0.2f)
+			return;
+		mLastTime = Time.realtimeSinceStartup;
+
+		if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePosition = (Vector2)Input.mousePosition;
+			PaintAt(mousePosition);
+        }
+	}
+
+    void PaintAt(Vector2 screenPosition)
+    {
+        Camera camera = Camera.main;
+        if (camera != null)
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(screenPosition);
+
+            if (Physics.Raycast(ray, out hit, float.PositiveInfinity, -1))
+            {
+                Point = hit.point;
+                Normal = -ray.direction;
+
+                Paintable[] paintables = hit.transform.GetComponentsInChildren<Paintable>();
+                for (int i = 0; i < paintables.Length; ++i)
+                {
+                    Paintable paintable = paintables[i];
+                    paintable.HandleHit(this);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Paintable.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChannelType
+{
+    UV0,
+    UV1,
+    UV2,
+    UV3,
+}
+
+public class Paintable : MonoBehaviour
+{
+    void OnEnable()
+    {
+        if (cachedMaterial == null)
+        {
+            Shader s = Shader.Find("Hidden/Paint in 3D/Decal");
+            cachedMaterial = new Material(s);
+        }
+        if (cachedMesh == null)
+        {
+            MeshFilter mf = GetComponent<MeshFilter>();
+            if (mf == null)
+            {
+                GameObject.Destroy(this);
+                return;
+            }
+            cachedMesh = mf.mesh;
+        }
+    }
+
+    public void HandleHit(PaintBrush brush)
+    {
+        Quaternion rotation = NormalToCameraRotation(brush.Normal);
+
+        float width = brush.mRadius;
+        float height = brush.mRadius;
+
+        if (brush.mDecal != null)
+        {
+            if (brush.mDecal.width > brush.mDecal.height)
+            {
+                height *= (float)brush.mDecal.height / (float)brush.mDecal.width;
+            }
+            else
+            {
+                width *= (float)brush.mDecal.width / (float)brush.mDecal.height;
+            }
+        }
+
+		Vector3 size = new Vector3(width, height, brush.mDepth);
+
+        cachedMatrix = Matrix4x4.TRS(brush.Point, rotation, size);
+        cachedDirection = rotation * Vector3.forward;
+        cachedBrush = brush;
+    }
+
+    Quaternion NormalToCameraRotation(Vector3 normal)
+    {
+        Camera camera = Camera.main;
+        Vector3 up = Vector3.up;
+
+        if (camera != null)
+        {
+            up = camera.transform.up;
+        }
+
+        return Quaternion.LookRotation(-normal, up);
+    }
+
+    void SetChannel(Material mat, ChannelType channel)
+    {
+        switch (channel)
+        {
+            case ChannelType.UV0:
+                {
+                    mat.SetVector("_Channel", new Vector4(1.0f, 0.0f, 0.0f, 0.0f));
+                }
+                break;
+            case ChannelType.UV1:
+                {
+                    mat.SetVector("_Channel", new Vector4(0.0f, 1.0f, 0.0f, 0.0f));
+                }
+                break;
+            case ChannelType.UV2:
+                {
+                    mat.SetVector("_Channel", new Vector4(0.0f, 0.0f, 1.0f, 0.0f));
+                }
+                break;
+            case ChannelType.UV3:
+                {
+                    mat.SetVector("_Channel", new Vector4(0.0f, 0.0f, 0.0f, 1.0f));
+                }
+                break;
+        }
+    }
+
+	Vector3 GetRev(Vector3 v)
+	{
+		Vector3 rst = new Vector3();
+		rst.x = v.x == 0.0f ? 0.0f : 1 / v.x;
+		rst.y = v.y == 0.0f ? 0.0f : 1 / v.y;
+		rst.z = v.z == 0.0f ? 0.0f : 1 / v.z;
+		return rst;
+	}
+
+    void LateUpdate()
+    {
+		if (cachedBrush == null)
+			return;
+
+        RenderTexture oldActive = RenderTexture.active;
+        RenderTexture.active = current;
+
+        SetChannel(cachedMaterial, ChannelType.UV0);
+		cachedMaterial.SetMatrix("_Matrix", cachedMatrix.inverse);
+        //cachedMaterial.SetTexture("_Buffer", );
+        cachedMaterial.SetVector("_Direction", cachedDirection);
+        cachedMaterial.SetColor("_Color", cachedBrush.mColor);
+		cachedMaterial.SetFloat("_Hardness", cachedBrush.mHandness);
+        cachedMaterial.SetTexture("_Texture", cachedBrush.mDecal);
+		cachedMaterial.SetFloat("_NormalScale", cachedBrush.mNormalScale);
+        cachedMaterial.SetPass(0);
+
+		Vector3 scaling = transform.lossyScale;
+		Matrix4x4 meshMatrix = transform.localToWorldMatrix * Matrix4x4.Scale(GetRev(scaling));
+
+        Graphics.DrawMeshNow(cachedMesh, meshMatrix, 0);
+
+        RenderTexture.active = oldActive;
+
+		cachedBrush = null;
+    }
+
+    Matrix4x4 cachedMatrix;
+    Vector3 cachedDirection;
+    PaintBrush cachedBrush;
+    Material cachedMaterial;
+
+    Mesh cachedMesh;
+
+    public RenderTexture current;
+}
